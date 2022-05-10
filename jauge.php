08a57d0ca80f718ce04b9dadb0d12d675aaf8d11
Lefhar <s.lefebvre907@laposte.net>
2022-05-10 11:16:58 +0200

    graphique en php bar avec line et jauge

--- jauge.php
+++ jauge.php
@@ -3,10 +3,10 @@ require_once ('jpgraph/jpgraph.php');
 require_once ('jpgraph/jpgraph_odo.php');
 
 // Create a new odometer graph (width=250, height=200 pixels)
-$graph = new OdoGraph(850,450);
+$graph = new OdoGraph(1050,650);
 
 $graph->title->Set('Chiffre d\'affaire');
-
+$graph->SetMargin(30,10,40,40);
 // Add drop shadow for graph
 $graph->SetShadow(false);
 $graph->SetColor('#FFFFF');
@@ -15,16 +15,17 @@ $graph->SetMarginColor("white");
 // Now we need to create an odometer to add to the graph.
 // By default the scale will be 0 to 100
 $odo = new Odometer(ODO_HALF);
-$odo->SetBorder('white',-9);
+//$odo->SetBorder('white',-5);
 //$odo->Set(FF_ARIAL,FS_NORMAL,14);
 
 //$odo->SetCenterAreaWidth(100);
 $odo->scale->label->SetColor('white');
-//$odo->scale->label->SetFont(FF_ARIAL,FS_BOLD,12);
+//$odo->scale->label->SetFont(FF_FONT2,FF_AHRON,50);
 // Add color indications
-$odo->AddIndication(0,90,"red");
-$odo->AddIndication(90,95,"orange");
-$odo->AddIndication(95,100,"green:0.7");
+$odo->AddIndication(0,90,"#D70606");
+$odo->AddIndication(90,95,"#FF6600");
+$odo->AddIndication(95,100,"#34E837");
+
 
 // Set display value for the odometer
 $odo->needle->Set(82);
--- jpgraph/jpgraph_odo.php
+++ jpgraph/jpgraph_odo.php
@@ -327,8 +327,8 @@ class OdoNeedle  {
             case NEEDLE_STYLE_LARGE_TRIANGLE: // Triangle medium width base
                 $base_width = isset($base_width)  ? $base_width : 25 ;
             case NEEDLE_STYLE_HUGE_TRIANGLE: // Triangle medium width base
-                $base_width = isset($base_width)  ? $base_width : 70 ;
-                $yadj = $base_width/1.8;
+                $base_width = isset($base_width)  ? $base_width : 34 ;
+                $yadj = $base_width/1;
                 $p = array($xc,$yc-$yadj,$xc+$r,$yc+$base_width/2-$yadj,$xc,$yc+$base_width-$yadj);
                 break;
 
